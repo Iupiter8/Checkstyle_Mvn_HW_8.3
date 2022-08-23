24f1ca2ff0eeeecf0f8fbc52f5d51fc212ac9778
Yurii Kulagin <yuk.178@bk.ru>
2022-08-23 20:43:26 +0300

    one test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,17 +18,17 @@ public class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
-    void findMaxIncomeMin() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
+//    @Test
+//    void findMaxIncomeMin() {
+//        StatisticsService service = new StatisticsService();
+//
+//        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+//        long expected = 12;
+//
+//        long actual = service.findMax(incomesInBillions);
+//
+//        assertEquals(expected, actual);
+//    }
 
 }
 
